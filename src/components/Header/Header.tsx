f54d788d823e09dcc7c14ab7d43a63e415263a11
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-04-16 16:05:13 +0900

    first commit && setting React+TS

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 10vh;
+  background: green;
+`;
+
+const Text = styled.div`
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: white;
+`;
+
+const Header: React.FC = () => {
+  return (
+    <Wrapper>
+      <Text>HEADER</Text>
+    </Wrapper>
+  );
+};
+
+export default Header;
